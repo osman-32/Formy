bdebd4ed02c16428b0b495298f2cf8c350afb257
Osman Yardimci <yardimciosman@gmail.com>
2020-07-09 20:58:55 -0500

    Hello Formy

---
+++ src/test/java/tests/Formy/PageScroll.java
@@ -0,0 +1,20 @@
+package tests.Formy;
+
+import org.testng.annotations.Test;
+import pages.FormyHomePage;
+import utils.Driver;
+
+import static utils.ReusableMethods.clickOnTheDesiredLink1;
+
+public class PageScroll {
+    @Test
+    public void verify_button_link_page() throws Exception {
+
+        FormyHomePage formyHomePage = new FormyHomePage();
+        Driver.get().get(formyHomePage.url);
+
+        clickOnTheDesiredLink1("Page Scroll");
+
+        Driver.get().close();
+    }
+}
